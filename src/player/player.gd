bb2fdfd8ffc66375ff9227d66be728c92fb767d5
Tyler Chase <tylercchase@gmail.com>
2025-07-31 20:09:47 -0400

    feat: move player forwards in rotation

---
+++ src/player/player.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@export var SPEED = 400
+
+var direction = Vector2(0,-1)
+var rotation_amount = 50
+
+var screen_size # Size of the game window.
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("move_right"):
+		rotate(-rotation_amount)
+	elif Input.is_action_just_pressed("move_left"):
+		rotate(rotation_amount)
+
+
+func _process(delta: float) -> void:
+	global_position = direction.rotated(rotation).normalized() * SPEED * delta

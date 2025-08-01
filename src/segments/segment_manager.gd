31ef83fce3100425bb6348ecb912b3a167fee21f
Tyler Chase <tylercchase@gmail.com>
2025-07-31 22:18:42 -0400

    feat: use line2D instead of scenes

--- src/segments/segment_manager.gd
+++ src/segments/segment_manager.gd
@@ -2,19 +2,15 @@ extends Node2D
 @export var segment_scene: PackedScene
 @export var player_node: Player
 
-var number_of_segments = 5
+@export var trail_line: Line2D
+
+var number_of_segments = 50
 
 var past_locations = []
 var segments: Array[Node2D] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for i in number_of_segments:
-		var segment = segment_scene.instantiate()
-		add_child(segment)
-		segment.global_position = player_node.global_position
-		segments.push_back(segment)
-		segments.reverse()
 	var timer = Timer.new()
 	timer.autostart = true
 	add_child(timer)
@@ -23,16 +19,11 @@ func _ready() -> void:
 
 # Used to create a new player node in global position after the timer runs out each time.
 func _on_timer_timeout():
-	past_locations.push_front({
-		"location": player_node.global_position,
-		"rotation": player_node.rotation
-		})
-	if len(past_locations) > number_of_segments:
-		past_locations.pop_back()
-	if len(segments) <= 0:
-		return
-	for i in len(past_locations):
-		segments[i].global_position = past_locations[i].location
-		segments[i].rotation = past_locations[i].rotation
+	trail_line.add_point(player_node.global_position)
+	if len(trail_line.points) > number_of_segments:
+		trail_line.remove_point(0)
+	check_closed_loops()
 
-
+func check_closed_loops():
+	
+	pass

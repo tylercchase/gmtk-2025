6b346aebca14a2c3dafa863a8ac1ea876b75433b
Tyler Chase <tylercchase@gmail.com>
2025-08-01 20:42:20 -0400

    feat: add other menus

---
+++ src/menus/game_over/game_over_menu.gd
@@ -0,0 +1,23 @@
+extends PanelContainer
+
+@export var play_again_button: Button
+@export var upgrade_button: Button
+@export var main_menu_button: Button
+
+@export_file("*.tscn") var level_scene: String
+@export_file("*.tscn") var upgrade_scene: String
+@export_file("*.tscn") var main_menu_scene: String
+
+func _ready() -> void:
+    play_again_button.pressed.connect(_on_play_again_button_pressed)
+    upgrade_button.pressed.connect(_on_upgrade_button_pressed)
+    main_menu_button.pressed.connect(_on_main_menu_button_pressed)
+
+func _on_play_again_button_pressed():
+    get_tree().change_scene_to_file(level_scene)
+
+func _on_upgrade_button_pressed():
+    get_tree().change_scene_to_file(upgrade_scene)
+
+func _on_main_menu_button_pressed():
+    get_tree().change_scene_to_file(main_menu_scene)

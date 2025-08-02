584f8a2378c32c64ef768800c7a84e3a213f38f2
Tyler Chase <tylercchase@gmail.com>
2025-08-01 21:10:47 -0400

    feat: timer for level time

--- src/level/level_manager.gd
+++ src/level/level_manager.gd
@@ -1,9 +1,13 @@
 extends Node
 
 @export var game_over_scene: PackedScene
+@export var progress_bar: ProgressBar
 
+@export var player: Player
+
+var timer: Timer
 func _ready() -> void:
-	var timer = Timer.new()
+	timer = Timer.new()
 	timer.autostart = true
 	timer.one_shot = true
 	add_child(timer)
@@ -13,3 +17,8 @@ func _ready() -> void:
 func _on_timer_timeout():
 	var game_over_node = game_over_scene.instantiate()
 	add_child(game_over_node)
+	player.process_mode = Node.PROCESS_MODE_DISABLED
+
+
+func _process(delta: float) -> void:
+	progress_bar.value =  timer.time_left / timer.wait_time

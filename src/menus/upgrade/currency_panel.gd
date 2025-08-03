5373780dd8e6389a42898cd5485f445e89a3247f
Tyler Chase <tylercchase@gmail.com>
2025-08-02 20:53:15 -0400

    feat: add menu buttons to upgrade screen

--- src/menus/upgrade/currency_panel.gd
+++ src/menus/upgrade/currency_panel.gd
@@ -2,6 +2,11 @@ extends PanelContainer
 
 @export var currency_label: RichTextLabel
 
+@export var main_menu_button: Button
+@export var level_button: Button
+
+@export_file("*.tscn") var main_menu_scene: String
+@export_file("*.tscn") var level_scene: String
 
 var template = ""
 
@@ -9,6 +14,14 @@ func _ready() -> void:
     template = currency_label.text
     update_text()
     Events.inventory_changed.connect(_on_inventory_changed)
+    main_menu_button.pressed.connect(_on_main_menu_button_pressed)
+    level_button.pressed.connect(_on_level_button_pressed)
+
+func _on_main_menu_button_pressed():
+    get_tree().change_scene_to_file(main_menu_scene)
+
+func _on_level_button_pressed():
+    get_tree().change_scene_to_file(level_scene)
 
 
 func _on_inventory_changed():
